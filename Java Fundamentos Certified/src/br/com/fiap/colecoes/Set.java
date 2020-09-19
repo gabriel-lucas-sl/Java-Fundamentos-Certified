6394bf3b889141e440baf5eabe942615e54b9a67
Gabriel Lucas <61299646+gabriel-lucas-sl@users.noreply.github.com>
2020-09-19 01:49:50 -0300

    Update Set.java

--- Java Fundamentos Certified/src/br/com/fiap/colecoes/Set.java
+++ Java Fundamentos Certified/src/br/com/fiap/colecoes/Set.java
@@ -1,4 +1,18 @@
 package br.com.fiap.colecoes;
 
 public class Set {
+  public static void main(String[] args) {
+        HashSet cursos = new HashSet();
+
+        cursos.add("Java");
+        cursos.add(".NET");
+        cursos.add("Android");
+
+        cursos.add("Java"); // Repetido!
+
+        System.out.println(cursos);
+
+
+
+    }
 }
